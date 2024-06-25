18b451620e3c234513fac444225dc825f602075f
Caio <caioviniciusleitelisboa@gmail.com>
2024-06-25 09:30:36 -0300

    feat: codigo de cancelar a consulta

--- main.js
+++ main.js
@@ -3,12 +3,12 @@ let consulta = {
   medico: "",
   data: "",
   hora: "",
-  cancelada: false,
 };
 let consultas = [];
+let deleteconsulta = [];
 let opcao = 0;
 
-console.log("Digite 1 para adicionar a consulta e insira seu nome. Digite 2 para listar todas as consultas agenddadas.");
+console.log("Digite 1 para adicionar a consulta e insira seu nome. Digite 2 para listar todas as consultas agendadas.");
 
 process.stdin.on("data", function (data) {
   entrada = data.toString().trim().toLowerCase();
@@ -36,7 +36,6 @@ process.stdin.on("data", function (data) {
           medico: "",
           data: "",
           hora: "",
-          cancelada: false,
         };
       }
     }
@@ -47,6 +46,23 @@ process.stdin.on("data", function (data) {
         } else {
             console.log("essas são as consultas atuais", consultas)
             opcao = 0 
+        }
+    }
+    if (opcao == 3){
+        let consultaRemover = data.toString().toLowerCase().trim()
+        let consultaEncontrada = false
+        consultas.forEach((consulte, index) =>{
+            if (consulte.nome.toLowerCase() == consultaRemover.toLowerCase()){
+                consultas.splice(index, 1);
+                consultaEncontrada = true
+            }
+        })
+        if (consultaEncontrada = true){
+            console.log("consulta removida")
+            opcao = 0
+        } else {
+            console.log("consulta não encontrada")
+            opcao = 0
         }
     }
   }
